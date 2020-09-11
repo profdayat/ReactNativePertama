add9a4418a7cfcbdb415c30d96a4c3e282fcddcd
Mukhammad Imam Nur Hidayat <dayat@mhs.stmik-yadika.ac.id>
2020-09-11 09:49:30 +0700

    Basic Layouting using FlexBox

---
+++ MateriFlexBox.js
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
+
+class MateriFlexBox extends Component {
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#c8d6e5',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Text>Beranda</Text>
+          <Text>Video</Text>
+          <Text>Playlist</Text>
+          <Text>Komunitas</Text>
+          <Text>Channel</Text>
+          <Text>Tentang</Text>
+        </View>
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+          <Image
+            source={{
+              uri:
+                'https://avatars1.githubusercontent.com/u/62937302?s=460&u=c39761c82208012c145774280eee29b671b600ba&v=4',
+            }}
+            style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+          />
+          <View>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Dayat</Text>
+            <Text>100 Subcriber</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default MateriFlexBox;
