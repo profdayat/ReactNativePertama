5128148bd71ddd3f2a606a2af2c3581e477413b8
Mukhammad Imam Nur Hidayat <dayat@mhs.stmik-yadika.ac.id>
2020-09-14 06:35:19 +0700

    Life Cycle Functional Component (Hooks)

--- src/pages/FlexBox/index.js
+++ src/pages/FlexBox/index.js
@@ -1,74 +1,133 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Text, View, Image} from 'react-native';
 
-class FlexBox extends Component {
-  constructor(props) {
-    super(props);
-    console.log('==> constructor');
-    this.state = {
-      subscriber: 200,
-    };
-  }
+// class FlexBox extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log('==> constructor');
+//     this.state = {
+//       subscriber: 200,
+//     };
+//   }
 
-  componentDidMount() {
-    console.log('==> component did mount');
+//   componentDidMount() {
+//     console.log('==> component did mount');
+//     setTimeout(() => {
+//       this.setState({
+//         subscriber: 400,
+//       });
+//     }, 4000);
+//   }
+
+//   componentDidUpdate() {
+//     console.log('==> component did update');
+//   }
+
+//   componentWillUnmount() {
+//     console.log('==> component will unmount');
+//   }
+
+//   render() {
+//     console.log('==> render');
+//     return (
+//       <View>
+//         <View
+//           style={{
+//             flexDirection: 'row',
+//             backgroundColor: '#c8d6e5',
+//             alignItems: 'center',
+//             justifyContent: 'space-between',
+//           }}>
+//           <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+//           <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+//         </View>
+//         <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+//           <Text>Beranda</Text>
+//           <Text>Video</Text>
+//           <Text>Playlist</Text>
+//           <Text>Komunitas</Text>
+//           <Text>Channel</Text>
+//           <Text>Tentang</Text>
+//         </View>
+//         <View
+//           style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+//           <Image
+//             source={{
+//               uri:
+//                 'https://avatars1.githubusercontent.com/u/62937302?s=460&u=c39761c82208012c145774280eee29b671b600ba&v=4',
+//             }}
+//             style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+//           />
+//           <View>
+//             <Text style={{fontSize: 20, fontWeight: 'bold'}}>Dayat</Text>
+//             <Text>{this.state.subscriber} Subcriber</Text>
+//           </View>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+const FlexBox = () => {
+  const [subscriber, setSubscriber] = useState(200);
+
+  useEffect(() => {
+    console.log('==> did mount');
     setTimeout(() => {
-      this.setState({
-        subscriber: 400,
-      });
-    }, 4000);
-  }
+      setSubscriber(400);
+    }, 2000);
+    return () => {
+      console.log('==> did update');
+    };
+  }, [subscriber]);
 
-  componentDidUpdate() {
-    console.log('==> component did update');
-  }
+  // useEffect(() => {
+  //   console.log('==> did update');
+  //   setTimeout(() => {
+  //     setSubscriber(400);
+  //   }, 2000);
+  // }, [subscriber]);
 
-  componentWillUnmount() {
-    console.log('==> component will unmount');
-  }
-
-  render() {
-    console.log('==> render');
-    return (
-      <View>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: '#c8d6e5',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}>
-          <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
-          <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
-        </View>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <Text>Beranda</Text>
-          <Text>Video</Text>
-          <Text>Playlist</Text>
-          <Text>Komunitas</Text>
-          <Text>Channel</Text>
-          <Text>Tentang</Text>
-        </View>
-        <View
-          style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
-          <Image
-            source={{
-              uri:
-                'https://avatars1.githubusercontent.com/u/62937302?s=460&u=c39761c82208012c145774280eee29b671b600ba&v=4',
-            }}
-            style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
-          />
-          <View>
-            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Dayat</Text>
-            <Text>{this.state.subscriber} Subcriber</Text>
-          </View>
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: '#c8d6e5',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+        <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+      </View>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <Text>Beranda</Text>
+        <Text>Video</Text>
+        <Text>Playlist</Text>
+        <Text>Komunitas</Text>
+        <Text>Channel</Text>
+        <Text>Tentang</Text>
+      </View>
+      <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+        <Image
+          source={{
+            uri:
+              'https://avatars1.githubusercontent.com/u/62937302?s=460&u=c39761c82208012c145774280eee29b671b600ba&v=4',
+          }}
+          style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+        />
+        <View>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Dayat</Text>
+          <Text>{subscriber} Subcriber</Text>
         </View>
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 export default FlexBox;
