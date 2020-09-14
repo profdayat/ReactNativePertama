c857123aeda72e0e4727aacb4a2e67c831fc41ea
Mukhammad Imam Nur Hidayat <dayat@mhs.stmik-yadika.ac.id>
2020-09-14 15:02:45 +0700

    make dinamic component with state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import StylingComponent from './pages/StylingComponent';
 import FlexBox from './pages/FlexBox';
 import Position from './pages/Position';
 import PropsDinamis from './pages/PropsDinamis';
+import StateDinamis from './pages/StateDinamis';
 
 const App = () => {
   const [isShow, SetIsShow] = useState(true);
@@ -20,7 +21,8 @@ const App = () => {
         {/* <StylingComponent /> */}
         {/* <FlexBox /> */}
         {/* <Position /> */}
-        <PropsDinamis />
+        {/* <PropsDinamis /> */}
+        <StateDinamis />
       </ScrollView>
     </View>
   );
---
+++ src/pages/StateDinamis/index.js
@@ -0,0 +1,65 @@
+import React, {Component, useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+const Counter = () => {
+  const [number, setNumber] = useState(0);
+  return (
+    <View>
+      <Text>{number}</Text>
+      <Button
+        title="Tambah"
+        onPress={() => {
+          setNumber(number + 1);
+        }}
+      />
+    </View>
+  );
+};
+
+class CounterClass extends Component {
+  state = {
+    number: 0,
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>{this.state.number}</Text>
+        <Button
+          title="Tambah"
+          onPress={() => this.setState({number: this.state.number + 1})}
+        />
+      </View>
+    );
+  }
+}
+
+const StateDinamis = () => {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.textTitle}>
+        Materi Component Dinamis dengan State
+      </Text>
+      <Text style={styles.textSection}>Functional Component (Hooks)</Text>
+      <Counter />
+      <Counter />
+      <Text style={styles.textSection}>Class Component</Text>
+      <CounterClass />
+      <CounterClass />
+    </View>
+  );
+};
+
+export default StateDinamis;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 20,
+  },
+  textTitle: {
+    textAlign: 'center',
+  },
+  textSection: {
+    marginTop: 20,
+  },
+});
